8dd2e5b3710fba8313c2dfd18f0c4966e306fe94
yama5993 <yama5993@nate.com>
2026-03-06 04:48:13 +0900

    슬라임 속도와 점프 속도를 조정하고 중력을 수정하여 게임 밸런스를 개선함

--- slimevolley/js/config.js
+++ slimevolley/js/config.js
@@ -14,8 +14,8 @@ const CONFIG = {
 
     // Slime
     SLIME_RADIUS: 38,
-    SLIME_SPEED: 2.5,
-    SLIME_JUMP_SPEED: -10,
+    SLIME_SPEED: 2,
+    SLIME_JUMP_SPEED: -9,
 
     // Ball
     BALL_RADIUS: 18,
@@ -25,7 +25,7 @@ const CONFIG = {
     BALL_SLIME_BOUNCE: 0.45,
 
     // Physics
-    GRAVITY: 0.25,
+    GRAVITY: 0.18,
 
     // Game rules
     MAX_SCORE: 15,
