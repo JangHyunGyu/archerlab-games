fa7bdbd9be6f621e37b1e1c4e87e73c68a702389
yama5993 <yama5993@nate.com>
2026-06-15 00:41:38 +0900

    운동선수 캐릭터의 사망 연출 크기 조정: 사망 크기 수정

--- school-zombie-defense/js/game.js
+++ school-zombie-defense/js/game.js
@@ -158,7 +158,7 @@
     elite: { corpseWidth: 0.96, deathSize: 1.05 },
     teacher: { corpseWidth: 0.84, deathSize: 0.96 },
     nurse: { corpseWidth: 1, deathSize: 1.02 },
-    athlete: { corpseWidth: 1.1, deathSize: 0.94 },
+    athlete: { corpseWidth: 1.1, deathSize: 1.1 },
     janitor: { corpseWidth: 1.34, deathSize: 1.12 },
     guard: { corpseWidth: 1.32, deathSize: 1.1 },
     crawler: { corpseWidth: 1.42, deathSize: 0.96 },
