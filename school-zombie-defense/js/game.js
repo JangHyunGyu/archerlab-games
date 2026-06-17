358bf84b1b3cc426db012e49c6935c3243fb8281
yama5993 <yama5993@nate.com>
2026-06-18 00:11:27 +0900

    간호사 시체 크기 조정: deathSize 값을 1.32에서 1.26으로 수정

--- school-zombie-defense/js/game.js
+++ school-zombie-defense/js/game.js
@@ -239,7 +239,7 @@
     volatile: { corpseWidth: 0.94, deathSize: 0.89 },
     elite: { corpseWidth: 0.96, deathSize: 1.05 },
     teacher: { corpseWidth: 0.98, deathSize: 1.12 },
-    nurse: { corpseWidth: 1.12, deathSize: 1.32 },
+    nurse: { corpseWidth: 1.12, deathSize: 1.26 },
     athlete: { corpseWidth: 1.1, deathSize: 1.24 },
     janitor: { corpseWidth: 1.34, deathSize: 1.32 },
     guard: { corpseWidth: 1.32, deathSize: 1.3 },
