9c5c2d416a651e03efc80485964548a12d067799
yama5993 <yama5993@nate.com>
2026-08-02 11:54:26 +0900

    Fix service worker response caching

--- slimevolley/sw.js
+++ slimevolley/sw.js
@@ -1,3 +1,3 @@
 self.__ARCHERLAB_GAME_ID__ = 'slimevolley-service-worker';
-importScripts('../shared/service-worker-error-reporter.js?v=20260710-d1-v2', '../shared/service-worker-runtime.js?v=20260802-runtime-v1');
+importScripts('../shared/service-worker-error-reporter.js?v=20260710-d1-v2', '../shared/service-worker-runtime.js?v=20260802-runtime-v2');
 self.ArcherGameServiceWorker.install({ gameId: 'slimevolley', version: '20260802-v1' });
